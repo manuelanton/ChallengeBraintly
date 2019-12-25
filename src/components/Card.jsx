649a470804bdc5046bd005acc6ea957a98732576
manuelanton <s.manuel.anton@gmail.com>
2019-12-25 16:30:18 -0300

    estilos pasables!!!!

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -16,52 +16,45 @@ import { addFav } from "../store/actions";
 
 const MovieCard = ({ movie, addFav, favorites }) => {
   return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateRows: "50% 25% 1fr 10%",
-        gridTemplateAreas: "image title subtitle button"
-      }}
-    >
+    <div style={{ textAlign: "center" }}>
       <Card>
         <Link to={`/movies/${movie.imdbID}`}>
-          <div
-            style={{ gridArea: "image", heigth: "80%", alignSelf: "center" }}
-          >
+          <div style={{ height: "80%", alignSelf: "center" }}>
             <CardImg top width="100%" height="200px" src={movie.Poster} />
           </div>
         </Link>
         <div style={{ height: "170px" }}>
           <CardBody>
-            <div
-              style={{ gridArea: "title", heigth: "80%", alignSelf: "center" }}
-            >
-              <CardTitle height="100px">{movie.Title}</CardTitle>
-            </div>
-            <div
-              style={{
-                gridArea: "subtitle",
-                heigth: "80%",
-                alignSelf: "center"
-              }}
-            >
-              <CardSubtitle>{movie.Year}</CardSubtitle>
-            </div>
-            {favorites.find(fav => fav.Title == movie.Title) ? (
-              "You love this movie!"
-            ) : (
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <div style={{ height: "48px", alignSelf: "center" }}>
+                <CardTitle height="100px">{movie.Title}</CardTitle>
+              </div>
+              <br />
               <div
-                stlye={{
-                  gridArea: "button",
-                  heigth: "80%",
+                style={{
+                  height: "80%",
                   alignSelf: "center"
                 }}
               >
-                <Button size="sm" onClick={e => addFav(movie)}>
-                  Add to Favorites
-                </Button>
+                <CardSubtitle>{movie.Year}</CardSubtitle>
+                <br />
               </div>
-            )}
+              {favorites.find(fav => fav.Title == movie.Title) ? (
+                "You love this movie!"
+              ) : (
+                <div
+                  style={{
+                    height: "80%",
+                    alignSelf: "center",
+                    marginTop: "auto"
+                  }}
+                >
+                  <Button size="sm" onClick={e => addFav(movie)}>
+                    Add to Favorites
+                  </Button>
+                </div>
+              )}
+            </div>
           </CardBody>
         </div>
       </Card>
